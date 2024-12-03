d64da64ef74beb3d94276e289035b552b763e950
Yukie Man <yukie.man97@gmail.com>
2024-12-03 00:54:03 -0800

    1: Finished day one

---
+++ src/main/java/dayone/LocationHintFinder.java
@@ -0,0 +1,53 @@
+package dayone;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+
+/**
+ * Day 1: Historian Hysteria
+ */
+public class LocationHintFinder {
+    public static void main(String[] args) throws FileNotFoundException {
+        InputStream inputStream = ClassLoader.getSystemResourceAsStream("input.txt");
+
+        List<Integer> l1 = new ArrayList<>();
+        List<Integer> l2 = new ArrayList<>();
+        Map<Integer, Integer> m = new HashMap<>();
+
+        try (Scanner scanner = new Scanner(inputStream, StandardCharsets.UTF_8)) {
+            while (scanner.hasNextLine()) {
+                int n1 = scanner.nextInt();
+                l1.add(n1);
+
+                int n2 = scanner.nextInt();
+                l2.add(n2);
+
+                if (m.containsKey(n2)) {
+                    m.compute(n2, (k, v) -> v + 1);
+                } else {
+                    m.put(n2, 1);
+                }
+            }
+        }
+
+        Collections.sort(l1);
+        Collections.sort(l2);
+
+        int distance = 0;
+        int similarityScore = 0;
+
+        for (int i = 0 ; i < l1.size(); i++) {
+            int n1 = l1.get(i);
+            int diff = Math.abs(n1 - l2.get(i));
+            distance += diff;
+
+            int subScore = m.getOrDefault(n1, 0) * n1;
+            similarityScore += subScore;
+        }
+
+        System.out.println("Distance: " + distance);
+        System.out.println("Similarity Score: " + similarityScore);
+    }
+}
